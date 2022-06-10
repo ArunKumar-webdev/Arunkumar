b56cbf6516ce1da87dff699cc892386eb05a9be0
ArunKumar <69241947+ArunKumar-webdev@users.noreply.github.com>
2022-06-10 18:04:49 +0530

    loader screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Home from './PortfolioContainer/home/Home'
+import "./App.css";
+import Home from "./PortfolioContainer/home/Home";
 
 function App() {
-  return <div className="App">
-    <Home />
-  </div>;
+  return (
+    <div className="App" style={{ height: "100vh" }}>
+      <Home style={{ height: "100vh" }} />
+    </div>
+  );
 }
 
 export default App;
--- src/PortfolioContainer/home/Home.js
+++ src/PortfolioContainer/home/Home.js
@@ -1,12 +1,34 @@
 import React from "react";
 import Profile from "./Profile/Profile";
 import Footer from "./Footer/Footer";
-import './Home.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Triangle } from "react-loader-spinner";
+import "./Home.css";
+import { useState, useEffect } from "react";
+
 export default function Home() {
-    return (
-        <div className="home-container">
-            <Profile />
-            <Footer /> 
-        </div>
-    )
+  const [spinner, setSpinner] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setSpinner(true), 1500);
+  }, []);
+
+  return !spinner ? (
+    <div
+      id="loader"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <Triangle color="#000000" className="m-auto" height={80} width={80} />
+    </div>
+  ) : (
+    <div style={{ display: "block" }} className="home-container">
+      <Profile />
+      <Footer />
+    </div>
+  );
 }
